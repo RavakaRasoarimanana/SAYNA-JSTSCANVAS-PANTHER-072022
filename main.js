c4b65723596972843ca7b875342c2baec401020e
Ravaka <ravakarasoa18@gmail.com>
2023-08-07 19:07:08 +0300

    animations CSS

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Page 1 et autorisation pour page2
+
 function nextPage1() {
     let answer1 = document.getElementById("answer1").value;
     if (answer1=="SI JE SUIS FIDELE C’EST A CE TRONE PEU IMPORTE QUI MONTE DESSUS"
@@ -11,7 +13,7 @@ function nextPage1() {
 }
     
 
-
+//Page 2 et autorisation pour page3
 function nextPage2() {
     let answer2 = document.getElementById("answer2").value;
     if (answer2=="Le film débute à Oackland en Californie. Il s'agit de la ville dont est originaire Ryan Coogler et également la ville qui a vu naître le mouvement politique des black panther"
@@ -23,6 +25,8 @@ function nextPage2() {
     } else {alert("Vérifies bien les orthographes et les accents.")
     }
 }
+//Page 3 et autorisation pour page résultat
+
 function nextPage3() {
     let answer3 = document.getElementById("answer3").value;
     if (answer3="LEROILION"|answer3=="leroilion"|answer3=="LE ROI LION"|answer3=="le roi lion") 
@@ -38,7 +42,7 @@ function nextPage3() {
 
 
 // Date cible du compte à rebours 
-const targetDate = new Date('2023-07-25 00:00:00').getTime();
+const targetDate = new Date('2023-08-14 00:00:00').getTime();
 
 // Mettre à jour le compte à rebours toutes les secondes
 const countdownInterval = setInterval(() => {
@@ -59,6 +63,6 @@ const countdownInterval = setInterval(() => {
   // Si le compte à rebours est terminé, afficher un message
   if (timeRemaining < 0) {
     clearInterval(countdownInterval);
-    document.getElementById('countdown').innerHTML = "Le compte à rebours est terminé !";
+    document.getElementById('countdown').innerHTML = "Le compte à rebours !";
   }
 }, 1000); 
